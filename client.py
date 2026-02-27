5ae3bec9b16d93b2ec589962544d7eb0d25145be
yanshizhao <79853429+yanshizhao@users.noreply.github.com>
2026-02-27 09:37:06 +0800

    Update comments for clarity in create_client function

--- client.py
+++ client.py
@@ -8,14 +8,13 @@ from config import ACCESS_KEY_ID, ACCESS_KEY_SECRET, REGION_ID, ENDPOINT
 def create_client():
     # 初始化配置
     config = open_api_models.Config(
-        # 从环境变量中获取 AccessKey
         access_key_id=ACCESS_KEY_ID,
         access_key_secret=ACCESS_KEY_SECRET,
         
-        # --- 关键：必须指定正确的 Endpoint ---
+        # --- 指定正确的 Endpoint ---
         endpoint=ENDPOINT,
         
-        # region_id 也建议指定
+        # 指定region_id 
         region_id=REGION_ID
     )
     return imageseg20191230Client(config)
